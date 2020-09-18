7ae58b84f746d7ed9d81c1c1a3bdd6115a5cd771
Boston <zhsegal@gmail.com>
2020-09-18 20:05:55 +0300

    before ejs

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}))
+app.set('views', path.join(__dirname, '/public/views'))
+app.set("view engine", "ejs")
+
 
 console.log((__dirname + '/public'))
 
@@ -45,7 +48,8 @@ app.post('/upload_video', (req, res) => {
         }
 
         // Display uploaded image for user validation
-        res.sendFile('public/show_hr.html', {root: __dirname })
+        // res.sendFile('public/show_hr.html', {root: __dirname })
+        res.render('vitalSign',{vitalSign:'try try try try'})
     });
 });
 
